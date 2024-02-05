75ea3d015ea3cce92472e7d0f60e0e0390d5f9cc
Dzmitry Panasevich <dimikgames@gmail.com>
2024-02-05 14:37:00 +0300

    создание сервиса для запроса данных с сервера

--- src/app/components/pages/products/products.component.ts
+++ src/app/components/pages/products/products.component.ts
@@ -19,15 +19,7 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     // this.products = this.productService.getProducts();
 
-    this.http.get<{ data: ProductType[] }>('http://testologia.size/pizzas?extraField=1')
-      .pipe(
-        tap(result => console.log(result)),
-        map(result => result.data),
-        retry(3),
-        catchError(err => {
-          return of([]);
-        }),
-      )
+    this.productService.getProducts()
       .subscribe({
         next: data => {
           this.products = data;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,73 +1,20 @@
 import { Injectable } from '@angular/core';
 import {ProductType} from "../types/product.type";
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
 
 @Injectable()
 export class ProductService {
 
-  private products: ProductType[] = [
-    {
-      id: 1,
-      image: 'pizza1.png',
-      title: 'Мясная Делюкс',
-      description: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы',
-      datetime: '2022-12-31 15:00:00'
-    },
-    {
-      id: 2,
-      image: '',
-      title: 'Морская Премиум',
-      description: 'Перец, сыр, креветки, кальмары, мидии, лосось',
-      datetime: '2022-10-31 15:00:00'
-    },
-    {
-      id: 3,
-      image: 'pizza3.png',
-      title: 'Бекон и Сосиски',
-      description: 'Бекон, сыр, сосиски, ананас, томатная паста',
-      datetime: '2023-12-31 15:00:00'
-    },
-    {
-      id: 4,
-      image: 'pizza4.png',
-      title: 'Куриная Делюкс',
-      description: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста',
-      datetime: '2022-12-31 11:44:31'
-    },
-    {
-      id: 5,
-      image: 'pizza5.png',
-      title: 'Барбекю Премиум',
-      description: 'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили',
-      datetime: '2022-12-31 15:00:00'
-    },
-    {
-      id: 6,
-      image: 'pizza6.png',
-      title: 'Пепперони Дабл',
-      description: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная',
-      datetime: '2022-12-31 15:00:00'
-    },
-    {
-      id: 7,
-      image: 'pizza7.png',
-      title: 'Куриное трио',
-      description: 'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы',
-      datetime: '2022-12-31 15:00:00'
-    },
-    {
-      id: 8,
-      image: 'pizza8.png',
-      title: 'Сырная',
-      description: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный',
-      datetime: '2022-01-31 15:00:00'
-    }
-  ];
+  private products: ProductType[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getProducts(): ProductType[] {
-    // ajax
-    return this.products;
+  getProducts(): Observable<ProductType[]> {
+    return this.http.get<{ data: ProductType[] }>('http://testologia.site/pizzas?extraField=1')
+      .pipe(
+        map(result => result.data)
+      );
   }
 
   getProduct(id: number): ProductType | undefined {
